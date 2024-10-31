aaa3642cad16cc148ddb1a1f73542a2f48bf1cab
Amadeo Terrière <amadeoterriere@imac.lan>
2024-10-31 21:56:03 +0100

    Setup basic authentication using SpotifyAPI and example code

--- TuneHunt/TuneHunt/TuneHuntApp.swift
+++ TuneHunt/TuneHunt/TuneHuntApp.swift
@@ -3,10 +3,17 @@ import SwiftData
 
 @main
 struct TuneHuntApp: App {
+    
+    @StateObject var spotify = Spotify()
+    
+    init() {
+//        SpotifyAPILogHandler.bootstrap()
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(spotify)
         }
     }
 }
